5446c719b797fcc546353154597df651aa90b07e
taherblk <taherblkhair99@gmail.com>
2026-08-08 22:31:27 +0200

    fix: comment out unused Tailwind CSS import and ensure newline at end of index.css

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from '@app/App';
 import '@shared/styles/index.css';
-
+// import '@shared/styles/tailwind.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
